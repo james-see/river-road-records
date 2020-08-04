5c1e5c1a5820a3f238725a50ae88bbd9bb1f4049
James Campbell <james@jamescampbell.us>
2020-08-04 00:23:57 -0400

    added all

--- gh-pages-site/gatsby-config.js
+++ gh-pages-site/gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pathPrefix: "/river-road-records",
+    pathPrefix: "/",
   siteMetadata: {
     title: `There are no coincidences...`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
