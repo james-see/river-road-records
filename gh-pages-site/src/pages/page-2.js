3a796166e92253ccb1d46c093d88f5e4ce0c3836
James Campbell <james@jamescampbell.us>
2020-08-04 14:17:36 -0400

    added josh band name

--- gh-pages-site/src/pages/page-2.js
+++ gh-pages-site/src/pages/page-2.js
@@ -10,6 +10,7 @@ const SecondPage = () => (
     <p><code>Twinklie-doo...</code></p>
     <ul>
       <li><a href="https://goldforlions.bandcamp.com/releases">Goldforlions</a></li>
+      <li><a href="https://soundcloud.com/josh-campbell">Josh Campbell</a> (latest <a href="https://soundcloud.com/josh-campbell/god-damned-miracle-demo">Failure Cross</a>)</li>
       <li></li>
     </ul>
     <Link to="/">Go back to the homepage</Link>
